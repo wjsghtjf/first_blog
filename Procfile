9db9540992eedbe0b7a2f3f6a978365e22218443
hosul <wjsghtjf@naver.com>
2019-03-05 19:54:07 +0900

    first commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn fproject.wsgi --log-file -
--- fproject/settings.py
+++ fproject/settings.py
@@ -20,10 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'vos%nhs(#mqny=f8p^o-wpi5j8odxz+2zp^gwifl5afn6=a&(!'
-
+#SECRET_KEY = 'vos%nhs(#mqny=f8p^o-wpi5j8odxz+2zp^gwifl5afn6=a&(!'
+import os
+SECRET_KEY= os.environ.get('DJANGO_SECRET_KEY', 'vos%nhs(#mqny=f8p^o-wpi5j8odxz+2zp^gwifl5afn6=a&(!')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = []
 
@@ -43,6 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -128,3 +131,7 @@ STATIC_ROOT=os.path.join(BASE_DIR,'static')
 
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 MEDIA_URL='/media/'
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
